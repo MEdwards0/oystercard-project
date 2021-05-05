212a66a4b94acaafcb96b8610f6e5e219ac582d3
Matthew <matt.edwards21@protonmail.com>
2021-05-05 16:32:53 +0100

    Refactored old code and up to record journeys

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
+require 'station.rb'
 
 class Oystercard
   attr_reader :balance
-  BALANCE_LIMIT = 100
+  BALANCE_LIMIT = 90
+  MINIMUM_FARE = 1
 
   def initialize
     @balance = 0
+    @touch_in = false
+    @touch_out = true
+    @in_journey = false
+    @log = []
   end
 
   def top_up(money)
-    raise 'Balance cannot exceed £100!' if bal_limit(top_up)
+    raise 'Balance cannot exceed £90!' if bal_limit(money)
 
     @balance += money
   end
@@ -18,7 +24,22 @@ class Oystercard
     value = @balance + money > BALANCE_LIMIT
   end
 
-  def ticket(fare)
+  def touch_in
+    fail 'Insufficient funds.' if @balance < MINIMUM_FARE
+  end
+
+  def touch_out
+    self.deduct(MINIMUM_FARE)
+  end
+
+  def in_journey?
+    
+  end
+
+  private
+
+  def deduct(fare)
     @balance -= fare
   end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -16,12 +16,36 @@ describe Oystercard do
     end
 
     it 'limits balance amount' do
-      expect { subject.top_up(101) }.to raise_error 'Balance cannot exceed £100!'
+      expect { subject.top_up(101) }.to raise_error 'Balance cannot exceed £90!'
     end
 
     it 'fare can be deducted' do
-      subject.top_up(100)
-      expect(subject.ticket(4)).to eq 96
+      subject.top_up(90)
+      subject.send(:deduct, 2) 
+      expect(subject.balance).to eq 88
     end
   end
+
+  context '#journey, #touch_in and #touch_out' do
+    it { is_expected.to respond_to :touch_in }
+
+    it { is_expected.to respond_to :touch_out }
+
+    it { is_expected.to respond_to :in_journey? }
+
+    it 'needs £1 minumum on card' do
+      expect{subject.touch_in}.to raise_error 'Insufficient funds.'
+    end
+
+    it 'deducts minimum balance on touch out' do
+      subject.top_up(2)
+      expect {subject.touch_out}.to change{subject.balance}.by(-Oystercard::MINIMUM_FARE)
+    end
+
+    # it 'stores station on touch in' do
+    #   station = double(:name = "King's Cross Station", :speed = 50) 
+    # end
+  end
 end
+
+# subject.send(:deduct, 2) 
